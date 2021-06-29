9b40417b0920db34989993493e6c3f99a62e38d3
Administrator <Administrator@SC-202012091212>
2021-06-29 14:40:00 +0900

    게시판

---
+++ green/src/main/java/com/pro/green/board/vo/ArticleVO.java
@@ -0,0 +1,62 @@
+package com.pro.green.board.vo;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.sql.Date;
+
+import org.springframework.stereotype.Component;
+
+@Component("articleVO")
+public class ArticleVO {
+	private int  noticeNum;
+	private String id;
+	private String noticeTitle;
+	private String noticeContent;
+	private Date  noticeDate;
+	
+	
+	public ArticleVO() {
+		System.out.println("ArticleVO »ý¼ºÀÚ");
+	}
+
+	public int getNoticeNum() {
+		return noticeNum;
+	}
+
+	public void setNoticeNum(int noticeNum) {
+		this.noticeNum = noticeNum;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getNoticeTitle() {
+		return noticeTitle;
+	}
+
+	public void setNoticeTitle(String noticeTitle) {
+		this.noticeTitle = noticeTitle;
+	}
+	
+	public String getNoticeContent() {
+		return noticeContent;
+	}
+
+	public void setNoticeContent(String noticeContent) {
+		this.noticeContent = noticeContent;
+	}
+	public Date getNoticeDate() {
+		return noticeDate;
+	}
+
+	public void setNoticeDate(Date noticeDate) {
+		this.noticeDate = noticeDate;
+	}
+		
+}
