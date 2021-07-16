178f332b2c84f9e5b11385a07ad0ed9674113613
Administrator <Administrator@SC-202012091212>
2021-07-16 14:50:00 +0900

    제발 부탁드려요 .

--- green/src/main/java/com/pro/green/product/service/ProductServiceImpl.java
+++ green/src/main/java/com/pro/green/product/service/ProductServiceImpl.java
@@ -18,7 +18,6 @@ import com.pro.green.product_M.vo.ProductVO2;
 public class ProductServiceImpl implements ProductService {
 	@Autowired
 	ProductDAO productDAO;
-<<<<<<< HEAD
 	
 	// 상품 목록
 	@Override
@@ -36,47 +35,19 @@ public class ProductServiceImpl implements ProductService {
 	}
 	
 	// 상품명&가격
-=======
 
->>>>>>> branch 'hw' of https://github.com/ahyeon-lee07/GreenTest.git
 	@Override
-<<<<<<< HEAD
+
 	public ProductVO2 viewProdDetail(String productId) throws DataAccessException {
 		return productDAO.viewProdDetail(productId);
 	}
 
-	@Override
-	public Map<String, Object> selectProdDetail(Map<String, Object> map) throws DataAccessException {
-		return productDAO.selectProdDetail(map);
-	}
-
-	// 상품 이미지
-=======
-	public List<ProductVO2> listProduct(String p_group) throws Exception {
-		List<ProductVO2> productsList = productDAO.selectAllProductsList(p_group);
-		return productsList;
-	}
-
-	// 상품 리스트 옵션
-	@Override
-	public List<Map<String, Object>> selectProdOption(String productId) throws DataAccessException {
-		return productDAO.selectProdOption(productId);
-	}
-
-	// 상품 상세페이지
-	@Override
-	public ProductVO2 viewProdDetail(String productId) throws DataAccessException {
-		return productDAO.viewProdDetail(productId);
-	}
-
-	// 상품 상세페이지
 	@Override
 	public Map<String, Object> selectProdDetail(Map<String, Object> map) throws DataAccessException {
 		return productDAO.selectProdDetail(map);
 	}
 
 	// 상품 이미지조회
->>>>>>> branch 'hw' of https://github.com/ahyeon-lee07/GreenTest.git
 	@Override
 	public List<Map<String, Object>> selectProdImg(String productId) throws DataAccessException {
 		return productDAO.selectProdImg(productId);
